a28ccf0391dd07174f22a26119535e7e0b510b64
lapphong <lapphong012@gmail.com>
2024-02-14 15:26:39 +0700

    add abstract repository

---
+++ lib/domain/repository/auth_repository.dart
@@ -0,0 +1,23 @@
+import '../domain.dart';
+
+abstract class AuthRepository {
+  Future<void> login({required String email, required String password});
+
+  Future<void> logout();
+
+  Future<void> resetPassword({
+    required String token,
+    required String email,
+    required String password,
+    required String confirmPassword,
+  });
+
+  Future<void> forgotPassword(String email);
+
+  Future<void> register({
+    required String username,
+    required String email,
+    required String password,
+    required Gender gender,
+  });
+}
