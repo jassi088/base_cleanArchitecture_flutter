888007aef19204779df1f0f50717582398635ee8
lapphong <lapphong012@gmail.com>
2024-02-14 15:29:21 +0700

    add des

--- lib/domain/repository/app_repository.dart
+++ lib/domain/repository/app_repository.dart
@@ -13,26 +13,6 @@ abstract class AppRepository {
 
   Stream<bool> get onConnectivityChanged;
 
-  // Future<void> login({required String email, required String password});
-
-  // Future<void> logout();
-
-  // Future<void> resetPassword({
-  //   required String token,
-  //   required String email,
-  //   required String password,
-  //   required String confirmPassword,
-  // });
-
-  // Future<void> forgotPassword(String email);
-
-  // Future<void> register({
-  //   required String username,
-  //   required String email,
-  //   required String password,
-  //   required Gender gender,
-  // });
-
   // User getUserPreference();
 
   Future<void> clearCurrentUserData();
@@ -40,8 +20,6 @@ abstract class AppRepository {
   Future<bool> saveIsFirstLogin(bool isFirstLogin);
 
   Future<bool> saveIsFirstLaunchApp(bool isFirstLaunchApp);
-
-  // Future<PagedList<User>> getUsers({required int page, required int? limit});
 
   Future<bool> saveIsDarkMode(bool isDarkMode);
 
@@ -53,7 +31,7 @@ abstract class AppRepository {
 
   Future<User> getMe();
 
-  /// ----- Store objectbox -----
+  /// ----- Store objectbox(*Local database*) -----
   int putLocalUser(User user);
 
   Stream<List<User>> getLocalUsersStream();
