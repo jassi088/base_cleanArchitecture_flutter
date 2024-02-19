f2c051aa95e3a2440af9ab9aeb1b512dd7fd74fc
lapphong <lapphong012@gmail.com>
2024-02-19 14:29:52 +0700

    rename presentation

---
+++ lib/presentation/navigation/app_navigator.iml.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+import 'package:flutter/src/animation/animation.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:injectable/injectable.dart';
+
+import '../../domain/domain.dart';
+import '../../shared/shared.dart';
+
+@LazySingleton(as: AppNavigator)
+class AppNavigatorImpl extends AppNavigator with LogMixin {
+  
+  
+}
---
+++ lib/presentation/navigation/mapper/app_route_info_mapper.dart
@@ -0,0 +1,19 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:injectable/injectable.dart';
+
+import '../../../domain/domain.dart';
+import '../../app.dart';
+
+@LazySingleton(as: BaseRouteInfoMapper)
+class AppRouteInfoMapper extends BaseRouteInfoMapper {
+  @override
+  PageRouteInfo map(AppRouteInfo appRouteInfo) {
+    return appRouteInfo.when(
+      login: () => const LoginRoute(),
+      main: () => const LoginRoute(),
+      itemDetail: (user) => const LoginRoute(),
+      // main: () => const MainRoute(),
+      // itemDetail: (user) => ItemDetailRoute(user: user),
+    );
+  }
+}
