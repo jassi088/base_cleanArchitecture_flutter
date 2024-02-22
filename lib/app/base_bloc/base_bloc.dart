c189dab999be796bf9b0e2dfd36f51bca61bc27e
lapphong <lapphong012@gmail.com>
2024-02-22 13:31:43 +0700

    add runBlocCatching in base_bloc

--- lib/app/base_bloc/base_bloc.dart
+++ lib/app/base_bloc/base_bloc.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../domain/domain.dart';
+import '../../presentation/presentation.dart';
 import '../../shared/shared.dart';
 import '../app.dart';
 
@@ -11,4 +15,109 @@ abstract class BaseBloc<E extends BaseBlocEvent, S extends BaseBlocState>
 abstract class BaseBlocDelegate<E extends BaseBlocEvent, S extends BaseBlocState>
     extends Bloc<E, S> {
   BaseBlocDelegate(super.initialState);
+
+  late final AppNavigator navigator;
+  late final AppBloc appBloc;
+  late final ExceptionHandler exceptionHandler;
+  late final ExceptionMessageMapper exceptionMessageMapper;
+  late final DisposeBag disposeBag;
+  late final CommonBloc _commonBloc;
+
+  set commonBloc(CommonBloc commonBloc) {
+    _commonBloc = commonBloc;
+  }
+
+  CommonBloc get commonBloc => this is CommonBloc ? this as CommonBloc : _commonBloc;
+
+  @override
+  void add(E event) {
+    if (!isClosed) {
+      super.add(event);
+    } else {
+      Log.e('Cannot add new event $event because $runtimeType was closed');
+    }
+  }
+
+  Future<void> addException(AppExceptionWrapper appExceptionWrapper) async {
+    commonBloc.add(ExceptionEmitted(appExceptionWrapper: appExceptionWrapper));
+
+    return appExceptionWrapper.exceptionCompleter?.future;
+  }
+
+  void showLoading() {
+    commonBloc.add(const LoadingVisibilityEmitted(isLoading: true));
+  }
+
+  void hideLoading() {
+    commonBloc.add(const LoadingVisibilityEmitted(isLoading: false));
+  }
+
+  Future<void> runBlocCatching({
+    required Future<void> Function() action,
+    Future<void> Function()? doOnRetry,
+    Future<void> Function(AppException)? doOnError,
+    Future<void> Function()? doOnSubscribe,
+    Future<void> Function()? doOnSuccessOrError,
+    Future<void> Function()? doOnEventCompleted,
+    bool handleLoading = true,
+    bool handleError = true,
+    bool handleRetry = true,
+    int? maxRetries,
+  }) async {
+    assert(maxRetries == null || maxRetries > 0, 'maxRetries must be positive');
+    Completer<void>? recursion;
+    try {
+      await doOnSubscribe?.call();
+      if (handleLoading) {
+        showLoading();
+      }
+
+      await action.call();
+
+      if (handleLoading) {
+        hideLoading();
+      }
+      await doOnSuccessOrError?.call();
+    } on AppException catch (e) {
+      if (handleLoading) {
+        hideLoading();
+      }
+      await doOnSuccessOrError?.call();
+      await doOnError?.call(e);
+
+      if (handleError || _forceHandleError(e)) {
+        await addException(AppExceptionWrapper(
+          appException: e,
+          doOnRetry: doOnRetry ??
+              (handleRetry && maxRetries != 1
+                  ? () async {
+                      recursion = Completer();
+                      await runBlocCatching(
+                        action: action,
+                        doOnEventCompleted: doOnEventCompleted,
+                        doOnSubscribe: doOnSubscribe,
+                        doOnSuccessOrError: doOnSuccessOrError,
+                        doOnError: doOnError,
+                        doOnRetry: doOnRetry,
+                        handleError: handleError,
+                        handleLoading: handleLoading,
+                        handleRetry: handleRetry,
+                        maxRetries: maxRetries?.minus(1),
+                      );
+                      recursion?.complete();
+                    }
+                  : null),
+          exceptionCompleter: Completer<void>(),
+        ));
+      }
+    } finally {
+      await recursion?.future;
+      await doOnEventCompleted?.call();
+    }
+  }
+
+  bool _forceHandleError(AppException appException) {
+    return appException is NetworkingException;
+    // && appException.networkExceptions == NetworkExceptions.refreshTokenFailed;
+  }
 }
