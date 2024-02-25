14b20e104ccccd3019db751b8f3fb747673f61a0
lapphong <lapphong012@gmail.com>
2024-02-25 21:36:02 +0700

    add runZonedGuarded in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,33 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
 import 'app/app.dart';
-import 'initializer/app_initializer.dart';
+import 'domain/domain.dart';
+import 'initializer/initializer.dart';
+import 'shared/shared.dart';
 
-void main() async {
+void main() => runZonedGuarded(_runMyApp, _reportError);
+
+Future<void> _runMyApp() async {
   WidgetsFlutterBinding.ensureInitialized();
   // await Firebase.initializeApp();
-  await AppInitializer().init();
+  await AppInitializer(InitializerConfig.getInstance()).init();
 
-  runApp(const MyApp());
+  final initialResource = await _loadInitialResource();
+  runApp(MyApp(initialResource: initialResource));
+}
+
+void _reportError(Object error, StackTrace stackTrace) {
+  Log.e(error, stackTrace: stackTrace, name: 'Uncaught exception');
+
+  // report by Firebase Crashlytics here
+}
+
+Future<List<InitialAppRoute>> _loadInitialResource() async {
+  final result = runCatching(action: () => getIt.get<AppUsecase>().loadInitialResourceUseCase);
+
+  return result.when(
+    success: (output) => output,
+    failure: (e) => const [InitialAppRoute.main],
+  );
 }
