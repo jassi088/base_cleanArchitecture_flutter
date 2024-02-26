e2cfb71840246e7624c023e1eb75ef16ae9e4422
lapphong <lapphong012@gmail.com>
2024-02-26 10:35:02 +0700

    add my_app

--- lib/app/my_app.dart
+++ lib/app/my_app.dart
@@ -1,115 +1,90 @@
-import 'package:flutter/foundation.dart';
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../domain/domain.dart';
-import '../initializer/di/di.dart';
+import '../initializer/initializer.dart';
+import '../presentation/presentation.dart';
+import '../shared/shared.dart';
+import 'app.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({required this.title, super.key});
-  final String title;
+class MyApp extends StatefulWidget {
+  const MyApp({required this.initialResource, super.key});
+  final List<InitialAppRoute> initialResource;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyApp> createState() => _MyAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyAppState extends BasePageState<MyApp, AppBloc> {
+  final _appRouter = getIt.get<AppRouter>();
 
-  Future<void> getData() async {
-    try {
-      await getIt.get<AuthUsecase>().loginUsecase(username: 'admin', password: '123456');
-    } catch (e) {
-      if (kDebugMode) {
-        print('❗❗Đã bắt lỗi nè ${e.toString()}');
-      }
-    }
-  }
+  @override
+  bool get isAppWidget => true;
 
   @override
   void initState() {
-    getData();
     super.initState();
-  }
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+    bloc.add(const AppInitiated());
   }
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+  Widget buildPage(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(DeviceConstants.designDeviceWidth, DeviceConstants.designDeviceHeight),
+      builder: (context, _) => BlocBuilder<AppBloc, AppState>(
+        buildWhen: (previous, current) => previous.isDarkTheme != current.isDarkTheme,
+        // || previous.languageCode != current.languageCode,
+        builder: (context, state) {
+          return MaterialApp.router(
+            builder: (context, child) {
+              final MediaQueryData data = MediaQuery.of(context);
+
+              return MediaQuery(
+                data: data.copyWith(textScaler: const TextScaler.linear(1.0)),
+                child: child ?? const SizedBox.shrink(),
+              );
+            },
+            routerDelegate: _appRouter.delegate(
+              deepLinkBuilder: (deepLink) {
+                return DeepLink(_mapRouteToPageRouteInfo());
+              },
+              navigatorObservers: () => [AppNavigatorObserver()],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+            routeInformationParser: _appRouter.defaultRouteParser(),
+            title: UiConstants.materialAppTitle,
+            color: UiConstants.taskMenuMaterialAppColor,
+            themeMode: state.isDarkTheme ? ThemeMode.dark : ThemeMode.light,
+            theme: lightTheme,
+            darkTheme: darkTheme,
+            debugShowCheckedModeBanner: false,
+            // localeResolutionCallback: (Locale? locale, Iterable<Locale> supportedLocales) =>
+            //     supportedLocales.contains(locale)
+            //         ? locale
+            //         : const Locale(LocaleConstants.defaultLocale),
+            // locale: Locale(state.languageCode.localeCode),
+            // supportedLocales: S.delegate.supportedLocales,
+            // localizationsDelegates: const [
+            //   S.delegate,
+            //   GlobalMaterialLocalizations.delegate,
+            //   GlobalWidgetsLocalizations.delegate,
+            //   GlobalCupertinoLocalizations.delegate,
+            // ],
+          );
+        },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  List<PageRouteInfo> _mapRouteToPageRouteInfo() {
+    return widget.initialResource.map<PageRouteInfo>((e) {
+      switch (e) {
+        case InitialAppRoute.login:
+          return const LoginRoute();
+        case InitialAppRoute.main:
+          return const MainRoute();
+      }
+    }).toList(growable: false);
   }
 }
