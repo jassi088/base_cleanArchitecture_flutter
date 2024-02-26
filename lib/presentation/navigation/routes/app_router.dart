82c69d9d5872d8bdd4d6613ab48dc7543f6703ca
lapphong <lapphong012@gmail.com>
2024-02-26 10:42:32 +0700

    add main tab in app_router

--- lib/presentation/navigation/routes/app_router.dart
+++ lib/presentation/navigation/routes/app_router.dart
@@ -1,7 +1,9 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:injectable/injectable.dart';
 
-import 'app_router.gr.dart';
+import '../../../domain/domain.dart';
+import '../../../initializer/initializer.dart';
+import '../../presentation.dart';
 
 @AutoRouterConfig(replaceInRouteName: 'Page,Route')
 @LazySingleton()
@@ -12,48 +14,48 @@ class AppRouter extends $AppRouter {
   @override
   List<AutoRoute> get routes => [
         AutoRoute(page: LoginRoute.page),
-        // AutoRoute(
-        //   page: MainRoute.page,
-        //   guards: [RouteGuard(getIt.get<AppUsecase>())],
-        //   children: [
-        //     AutoRoute(
-        //       page: HomeTab.page,
-        //       maintainState: true,
-        //       children: [
-        //         AutoRoute(page: HomeRoute.page, initial: true),
-        //         // AutoRoute(page: ItemDetailRoute.page),
-        //       ],
-        //     ),
-        //     AutoRoute(
-        //       page: SearchTab.page,
-        //       maintainState: true,
-        //       children: [
-        //         AutoRoute(page: SearchRoute.page, initial: true),
-        //       ],
-        //     ),
-        //     AutoRoute(
-        //       page: SettingTab.page,
-        //       maintainState: true,
-        //       children: [
-        //         AutoRoute(page: SettingRoute.page, initial: true),
-        //       ],
-        //     ),
-        //   ],
-        // ),
+        AutoRoute(
+          page: MainRoute.page,
+          guards: [RouteGuard(getIt.get<AppUsecase>())],
+          children: [
+            AutoRoute(
+              page: HomeTab.page,
+              maintainState: true,
+              children: [
+                AutoRoute(page: HomeRoute.page, initial: true),
+                // AutoRoute(page: ItemDetailRoute.page),
+              ],
+            ),
+            AutoRoute(
+              page: SearchTab.page,
+              maintainState: true,
+              children: [
+                AutoRoute(page: SearchRoute.page, initial: true),
+              ],
+            ),
+            AutoRoute(
+              page: SettingTab.page,
+              maintainState: true,
+              children: [
+                AutoRoute(page: SettingRoute.page, initial: true),
+              ],
+            ),
+          ],
+        ),
       ];
 }
 
-// @RoutePage(name: 'HomeTab')
-// class HomeTabPage extends AutoRouter {
-//   const HomeTabPage({super.key});
-// }
+@RoutePage(name: 'HomeTab')
+class HomeTabPage extends AutoRouter {
+  const HomeTabPage({super.key});
+}
 
-// @RoutePage(name: 'SearchTab')
-// class SearchTabPage extends AutoRouter {
-//   const SearchTabPage({super.key});
-// }
+@RoutePage(name: 'SearchTab')
+class SearchTabPage extends AutoRouter {
+  const SearchTabPage({super.key});
+}
 
-// @RoutePage(name: 'SettingTab')
-// class SettingTabPage extends AutoRouter {
-//   const SettingTabPage({super.key});
-// }
+@RoutePage(name: 'SettingTab')
+class SettingTabPage extends AutoRouter {
+  const SettingTabPage({super.key});
+}
