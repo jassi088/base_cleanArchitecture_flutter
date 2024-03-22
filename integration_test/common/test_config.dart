7928d6ff5c4bdd4cf4fb93f9536407ab4ed25532
lapphong <lapphong012@gmail.com>
2024-03-22 09:07:27 +0700

    add configure integration_test

---
+++ integration_test/common/test_config.dart
@@ -0,0 +1,9 @@
+class TestConfig {
+  const TestConfig._();
+
+  static const androidScreenShotsFolder = 'integration_test/screenshots/android';
+  static const iosScreenShotsFolder = 'integration_test/screenshots/ios';
+  static const webScreenShotsFolder = 'integration_test/screenshots/web';
+  static const timeoutInMillis = 10000;
+  static const delayInMillis = 100;
+}
